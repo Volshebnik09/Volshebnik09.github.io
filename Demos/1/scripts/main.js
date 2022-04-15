2c0650dea05e0c08aab680eede698c091b66300c
Komnatniy_Koldun <82974466+Volshebnik09@users.noreply.github.com>
2022-04-15 09:49:15 +0300

    Update main.js

--- Demos/1/scripts/main.js
+++ Demos/1/scripts/main.js
@@ -1,9 +1,9 @@
-// $(document).ready(function(){
-//       $('.callback__cards-holder').slick({
-//           slidesToShow: 3,
-//           slidesToScroll: 1,
-//           variableWidth: true,
-//           nextArrow: document.querySelector('.slick-next'),
-//           prevArrow: document.querySelector('.slick-prev')
-//       });
-//     });
+$(document).ready(function(){
+      $('.callback__cards-holder').slick({
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          variableWidth: true,
+          nextArrow: document.querySelector('.slick-next'),
+          prevArrow: document.querySelector('.slick-prev')
+      });
+    });
