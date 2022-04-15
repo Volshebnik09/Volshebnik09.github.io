c4c7eb6466486b724045f371871bed2d8c86242e
Komnatniy_Koldun <82974466+Volshebnik09@users.noreply.github.com>
2022-04-15 11:24:28 +0300

    update

---
+++ scripts/workExamplesMachine.js
@@ -0,0 +1,19 @@
+let cards = document.querySelectorAll('.my-sites__card')
+
+let bool = false;
+cards.forEach((item) => {
+  item.querySelector('.my-sites__card__img').addEventListener('click',()=>{
+  	if (item.hasAttribute('active')) 
+  		bool=true;
+  	cards.forEach((otheritem)=>{
+  		otheritem.removeAttribute('active','');
+  	})
+  	item.hasAttribute('active')? 
+  	item.removeAttribute('active',''):
+  	item.setAttribute('active','')
+
+  	if (bool) 
+  		item.removeAttribute('active','')
+  	bool=false
+  })
+})
