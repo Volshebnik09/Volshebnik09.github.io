2bed0eed078a2d3f36c83348a9380412b14b8583
Fridge <shulgin.grigoriy@yandex.ru>
2022-05-13 17:22:39 +0300

    first commit

--- scripts/bulbMachine.js
+++
@@ -1,9 +0,0 @@
-let bulb = document.querySelector('.bulb-on');
-
-bulb.classList.toggle('bulb-is-off')
-
-bulb.addEventListener('click',(e)=>{
-	e.preventDefault()
-	bulb.classList.toggle('bulb-is-on')
-	bulb.classList.toggle('bulb-is-off')
-})
